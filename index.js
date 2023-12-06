ca9b52bf7fce1e3f78f3f0b783dedc90512ea7e8
Izzamuddin Royhul Firdaus <dimanton1221@gmail.com>
2023-12-06 07:31:04 +0700

    fix vehicle

--- index.js
+++ index.js
@@ -21,43 +21,16 @@ const mqttc = mqtt.connect('mqtt://127.0.0.1:1883');
 mqttc.subscribe('Parkir/1');
 mqttc.subscribe('Parkir/2');
 
-// pokoknya itu kalau pesan masuk 
-// mqttc.on('message', function (topic, message) {
-//     // message is Buffer
-//     console.log(topic.toString() + " " + message.toString());
-//     io.emit('message', message.toString());
-//     // client.end()
-// })
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/parkirsek', (req, res) => {
-    res.sendFile(__dirname + '/buatanIzzam.html')
-})
-
-
-app.get('/parkir', (req, res) => {
-    res.sendFile(__dirname + '/apa.html')
-})
-
-app.get('/css.css', (req, res) => {
-    res.sendFile(__dirname + '/parkir.css')
-})
-
-
 io.on('connection', (socket) => {
-    // console.log("ada yang konek");
-
     mqttc.on('message', function (topic, message) {
-        // message is Buffer
-        // console.log("ngirim coy");
         socket.emit(topic.toString(), message.toString());
-
     });
-
-
     socket.on('disconnect', () => {
         console.log('arek e ilang cuy');
     });
